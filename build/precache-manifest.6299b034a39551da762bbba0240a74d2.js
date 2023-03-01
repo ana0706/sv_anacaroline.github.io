f4cb4b90bed972ba7a80e3d487bedc841a9dbbf4
ana0706 <sv_anacaroline@live.com>
2023-03-01 19:35:21 -0300

    chore: update distribution

---
+++ build/precache-manifest.6299b034a39551da762bbba0240a74d2.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5aab14902a55f6bf71bdf0fdaf8af913",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c10e1f75158fe56d6cf",
+    "url": "/static/css/main.495a3c44.chunk.css"
+  },
+  {
+    "revision": "05e2978154cd37795e71",
+    "url": "/static/js/2.8ca78770.chunk.js"
+  },
+  {
+    "revision": "2c10e1f75158fe56d6cf",
+    "url": "/static/js/main.f4feb64e.chunk.js"
+  },
+  {
+    "revision": "595a508e15c85949a698",
+    "url": "/static/js/runtime-main.7e561a75.js"
+  },
+  {
+    "revision": "516838f0b04a679134c28be71fff2562",
+    "url": "/static/media/eu.516838f0.jpeg"
+  },
+  {
+    "revision": "010b604f84431eb7cdbc6620aa64cb55",
+    "url": "/static/media/firacode-bold.010b604f.woff2"
+  },
+  {
+    "revision": "22cf339e034bb9a3d21c0f7c6c11fade",
+    "url": "/static/media/firacode-bold.22cf339e.woff"
+  },
+  {
+    "revision": "f49305f3ff86f0f33e04658fa795c95c",
+    "url": "/static/media/firacode-bold.f49305f3.otf"
+  },
+  {
+    "revision": "004ca30540f4e610737895585f14a6b5",
+    "url": "/static/media/firacode-light.004ca305.woff"
+  },
+  {
+    "revision": "bc4d306ac11284a7d5460b426f3ff739",
+    "url": "/static/media/firacode-light.bc4d306a.otf"
+  },
+  {
+    "revision": "f8f87b9826c9d68e6666cc483bd0451c",
+    "url": "/static/media/firacode-light.f8f87b98.woff2"
+  },
+  {
+    "revision": "551a04f3a6544dcb9468992a3d87a2b0",
+    "url": "/static/media/firacode-medium.551a04f3.woff"
+  },
+  {
+    "revision": "7064d44fb7d3128f4958b8b907ef1d83",
+    "url": "/static/media/firacode-medium.7064d44f.otf"
+  },
+  {
+    "revision": "f18fd1b3cc4fc5265dc28c68947294a4",
+    "url": "/static/media/firacode-medium.f18fd1b3.woff2"
+  }
+]);
